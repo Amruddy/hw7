565683a31dbeec7d74bbb06eef5f9851a528d8e6
Amruddy <embaev94@gmail.com>
2021-12-13 16:25:58 +0300

    hw7

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+    public static void main(String[] args) {
+       Cat[] cats = new Cat[]{
+               new Cat("Misov"),
+               new Cat("Pisov"),
+               new Cat("Dechk")
+       };
+       Plate plate = new Plate(100);
+
+        for (int i = 0; i < cats.length; i++) {
+            cats[i].eat(plate);
+        }
+        for (int i = 0; i < cats.length; i++) {
+            System.out.println(cats[i]);
+        }
+
+    }
+
+
+
 }
